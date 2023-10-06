9093212e28ee9f17d40a80a8f4715ce71a1a1bbe
文吉平 <7005763@qq.com>
2023-10-06 23:11:27 +0800

    Update vast.sh

--- vast.sh
+++ vast.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xEE63189A3B5d35fF8E6f67BD2B2A3ACa4E084e85/g' config.conf
 sleep 5
 echo "---------------------------"
 

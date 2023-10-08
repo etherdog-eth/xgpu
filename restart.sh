a42386c8ba2ac6c4ba35d75f258b1a62d8607b46
文吉平 <7005763@qq.com>
2023-10-08 15:25:45 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,16 +2,15 @@ echo "正在停止挖矿程序"
 ps aux|grep miner|grep -v grep | awk '{print $2}'|xargs kill -9
 echo "正在启动挖矿程序"
 
-cd /root/XENGPUMiner
+cd XENGPUMiner
 
 sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
 
-#count=`nvidia-smi | grep NVIDIA | grep -v NVIDIA-SMI -c | awk '{print $1}'`
-#echo "GPU count:$count"
+count=`nvidia-smi | grep NVIDIA | grep -v NVIDIA-SMI -c`
+echo "GPU数量:$count"
 
-for k in $(nvidia-smi | grep NVIDIA | grep -v NVIDIA-SMI -c | awk '{print $1}')
+for k in $(nvidia-smi | grep NVIDIA | grep -v NVIDIA-SMI | awk '{print $2}')
 do
     cmd="sudo nohup ./xengpuminer -d$k > xengpuminer-$k.log 2>&1 &"
-    echo cmd
-    ${cmd}
+    eval $cmd
 done
